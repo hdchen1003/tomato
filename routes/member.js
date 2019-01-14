b2fa600ebe8f8634bdaa9ecd4c33d5cd23a99114
hdchen <hdchen1003@gmail.com>
2019-01-14 16:42:09 +0800

    sss

--- routes/member.js
+++ routes/member.js
@@ -5,7 +5,8 @@ var Today=new Date();
 /* GET home page. */
 
 router.get('/profile', function (req, res, next) {
-    var todo_count = 0
+    if (req.cookies.status) {
+        var todo_count = 0
     var done_count = 0
     admin.ref('user/' + req.cookies.status.unumber + '/todoList').once('value', function (snapshop) {
         data = snapshop.val()
@@ -35,12 +36,23 @@ router.get('/profile', function (req, res, next) {
             }
         }
     })
+    }
+    else {
+        res.render('nonlog', { title: '尚未登入' });
+    }
+   
 });
 router.get('/todoList', function (req, res, next) {
-    admin.ref('user/' + req.cookies.status.unumber + '/todoList').once('value', function (snapshop) {
-        data = snapshop.val()
-        res.render('todoList', { title: '待辦事項', data: data ,unumber:req.cookies.status.unumber,  target: req.cookies.status.target,ccc: req.cookies.status.count});
-    })
+    if (req.cookies.status) {
+        admin.ref('user/' + req.cookies.status.unumber + '/todoList').once('value', function (snapshop) {
+            data = snapshop.val()
+            res.render('todoList', { title: '待辦事項', data: data ,unumber:req.cookies.status.unumber,  target: req.cookies.status.target,ccc: req.cookies.status.count});
+        })
+    }
+    else {
+        res.render('nonlog', { title: '尚未登入' });
+    }
+    
 })
 router.get('/addtodoList', function (req, res, next) {
     res.render('addtodoList', { title: '新增待辦事項' });
@@ -55,11 +67,17 @@ router.post('/do_addtodoList', function (req, res, next) {
 })
 
 router.get('/doneList', function (req, res, next) {
-    admin.ref('user/' + req.cookies.status.unumber + '/doneList').once('value', function (snapshop) {
-        data = snapshop.val()
-        console.log(req.cookies.status.count)
-        res.render('doneList', { title: '完成事項', data: data,  target: req.cookies.status.target, ccc:req.cookies.status.count });
-    })
+    if (req.cookies.status) {
+        admin.ref('user/' + req.cookies.status.unumber + '/doneList').once('value', function (snapshop) {
+            data = snapshop.val()
+            console.log(req.cookies.status.count)
+            res.render('doneList', { title: '完成事項', data: data,  target: req.cookies.status.target, ccc:req.cookies.status.count });
+        })
+    }
+    else {
+        res.render('nonlog', { title: '尚未登入' });
+    }
+   
 })
 
 router.post('/do_adddoneList', function (req, res, next) {
@@ -70,9 +88,17 @@ router.post('/do_adddoneList', function (req, res, next) {
         create.set({ mission: _data.mission, count: _data.count,date:(Today.getMonth()+1)+'/'+Today.getDate() })
         var remove = admin.ref('user/' + req.cookies.status.unumber + '/todoList/' + req.body.item)
         remove.remove()
+        var count = req.cookies.status.count + _data.count
+        res.cookie('status', {
+            'uid': req.cookies.status.uid,
+            'uname': req.cookies.status.uname,
+            'unumber':req.cookies.status.unumber,
+            'target':req.cookies.status.target,
+            'count':count    
+        })
         admin.ref('user/' + req.cookies.status.unumber + '/todoList').once('value', function (snapshop) {
             __data = snapshop.val()
-            res.render('todoList', { title: '待辦事項', data: __data,unumber: req.cookies.status.unumber,ccc: req.cookies.status.count});
+            res.render('todoList', { title: '待辦事項', data: __data,unumber: req.cookies.status.unumber,target: req.cookies.status.target,ccc: req.cookies.status.count});
         })
     })
     
